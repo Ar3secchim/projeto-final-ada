59b4716cb7d8324a10568485e3923757192d991f
Renara Secchim <renarasecchim@gmail.com>
2024-09-07 08:44:33 -0300

    feat: :tada: Add initial project files and configurations

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-slate-50">
+      <header className="text-red-700">
+        <h1>React App</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
